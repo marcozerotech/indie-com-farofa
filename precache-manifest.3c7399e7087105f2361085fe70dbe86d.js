a7fc83f6b1d3901b37210cdc5c46d68c2347d84e
Victoria Luquet <victoria.tewari@gmail.com>
2020-08-19 13:00:14 -0300

    Updates

---
+++ precache-manifest.3c7399e7087105f2361085fe70dbe86d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0c8e018e36f179cee51cfb1ac80fd0",
+    "url": "/indie-com-farofa/index.html"
+  },
+  {
+    "revision": "f9e763df7042cf5112d8",
+    "url": "/indie-com-farofa/static/css/main.805d5014.chunk.css"
+  },
+  {
+    "revision": "303b020098904f55a561",
+    "url": "/indie-com-farofa/static/js/2.80bcef85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/indie-com-farofa/static/js/2.80bcef85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e763df7042cf5112d8",
+    "url": "/indie-com-farofa/static/js/main.b5c785c1.chunk.js"
+  },
+  {
+    "revision": "3499c3a388769ae3bc56",
+    "url": "/indie-com-farofa/static/js/runtime-main.8df268de.js"
+  },
+  {
+    "revision": "f62cd7adaabf512d2092d6802b7e8756",
+    "url": "/indie-com-farofa/static/media/colorfull-logo.f62cd7ad.jpg"
+  }
+]);
